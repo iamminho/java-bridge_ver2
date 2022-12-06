f2eb3578286f17d33d8efe64acd9f316645c4520
iamminho <minho100227@kakao.com>
2022-12-06 23:54:26 +0900

    feat: 사용자의 입력을 반환하는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,28 +1,32 @@
-package bridge;
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.\n";
+    private final String INPUT_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final String INPUT_GAME_COMMAD = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private final String INVALID_INPUT_BRIDGE = "[ERROR] 3과 20사이의 숫자만 입력해주세요";
 
-    /**
-     * 다리의 길이를 입력받는다.
-     */
     public int readBridgeSize() {
-        return 0;
+        try {
+            System.out.println(INPUT_BRIDGE_SIZE);
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException error) {
+            throw new IllegalArgumentException(INVALID_INPUT_BRIDGE);
+        }
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        return null;
+        System.out.println(INPUT_MOVING);
+        return Console.readLine();
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        return null;
+        System.out.println(INPUT_GAME_COMMAD);
+        return Console.readLine();
     }
 }
