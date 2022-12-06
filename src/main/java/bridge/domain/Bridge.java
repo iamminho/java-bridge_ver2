f24832946acf2ff4086dc688bb3ef30140174be0
iamminho <minho100227@kakao.com>
2022-12-07 00:01:24 +0900

    refactor: 매개변수 예외처리 기능 추가

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -5,12 +5,28 @@ import bridge.domain.dto.MovingResult;
 import java.util.List;
 
 public class Bridge {
+    private final String INVALID_DIRECTION = "[ERROR] 다리의 각 칸은 U또는 D로 이루어져야 합니다.";
+    private final String UP_DIRECTION = "U";
+    private final String DOWN_DIRECTION = "D";
     private List<String> bridge;
     private BridgeLocation bridgeLocation;
 
+
     public Bridge(List<String> bridge, BridgeLocation bridgeLocation) {
+        validateBridge(bridge);
         this.bridge = bridge;
         this.bridgeLocation = bridgeLocation;
+    }
+
+    private void validateBridge(List<String> bridge) {
+        bridge.stream()
+                .forEach(space -> validateDirection(space));
+    }
+
+    private void validateDirection(String direction) {
+        if (!direction.equals(UP_DIRECTION) && !direction.equals(DOWN_DIRECTION)) {
+            throw new IllegalArgumentException(INVALID_DIRECTION);
+        }
     }
 
     public MovingResult getMovingResult(Moving moving) {
@@ -25,5 +41,9 @@ public class Bridge {
     private String getDirection(List<String> bridge) {
         bridgeLocation.moveLocation();
         return bridgeLocation.getCurrentDirection(bridge);
+    }
+
+    public void resetLocation() {
+        bridgeLocation.resetLocation();
     }
 }
